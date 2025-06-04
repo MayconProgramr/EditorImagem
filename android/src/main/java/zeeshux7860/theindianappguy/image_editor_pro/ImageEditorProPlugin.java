3aaf20fdfcc7eac1a8a2b44143b2fa347f7f99ef
MayconProgramr <may.con.00@hotmail.com>
2025-06-04 16:37:48 -0300

    Atualizado versão do Flutter para 3.32.0

--- android/src/main/java/zeeshux7860/theindianappguy/image_editor_pro/ImageEditorProPlugin.java
+++ android/src/main/java/zeeshux7860/theindianappguy/image_editor_pro/ImageEditorProPlugin.java
@@ -6,28 +6,15 @@ import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
-import io.flutter.plugin.common.PluginRegistry.Registrar;
 
 /** ImageEditorProPlugin */
 public class ImageEditorProPlugin implements FlutterPlugin, MethodCallHandler {
+  private MethodChannel channel;
+
   @Override
   public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
-    final MethodChannel channel = new MethodChannel(flutterPluginBinding.getFlutterEngine().getDartExecutor(), "image_editor_pro");
-    channel.setMethodCallHandler(new ImageEditorProPlugin());
-  }
-
-  // This static function is optional and equivalent to onAttachedToEngine. It supports the old
-  // pre-Flutter-1.12 Android projects. You are encouraged to continue supporting
-  // plugin registration via this function while apps migrate to use the new Android APIs
-  // post-flutter-1.12 via https://flutter.dev/go/android-project-migration.
-  //
-  // It is encouraged to share logic between onAttachedToEngine and registerWith to keep
-  // them functionally equivalent. Only one of onAttachedToEngine or registerWith will be called
-  // depending on the user's project. onAttachedToEngine or registerWith must both be defined
-  // in the same class.
-  public static void registerWith(Registrar registrar) {
-    final MethodChannel channel = new MethodChannel(registrar.messenger(), "image_editor_pro");
-    channel.setMethodCallHandler(new ImageEditorProPlugin());
+    channel = new MethodChannel(flutterPluginBinding.getBinaryMessenger(), "image_editor_pro");
+    channel.setMethodCallHandler(this);
   }
 
   @Override
@@ -41,5 +28,9 @@ public class ImageEditorProPlugin implements FlutterPlugin, MethodCallHandler {
 
   @Override
   public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
+    if (channel != null) {
+      channel.setMethodCallHandler(null);
+      channel = null;
+    }
   }
 }
